7eda4a0c2bb4845ae625b2d889dbf1b0f52bf203
antaresshipyard <73565991+antaresshipyard@users.noreply.github.com>
2024-03-02 19:57:10 -0500

    Update admirals.js

--- src/data/admirals.js
+++ src/data/admirals.js
@@ -6,6 +6,23 @@ module.exports = [
 		set: [
 			"75016"
 		],
+		name: "Test",
+		unique: true,
+		text: "Add 2 [crew_text] to this ship's Upgrade Bar <hr> <b>WHEN DEFENDING:</b> If this ship is within Range of a Non-Minefield Obstacle. \n\n Roll +1 defense die. In addition if the attacking ship is either a <img src='img/text/faction-klingon.png' width='20' height='20' title='Klingon'> ship or <img src='img/text/faction-federation.png' width='20' height='20' title='Federation'> ship, you may re-roll 1 [blank]. <hr> <b>GATHER FORCES STEP:</b> If this card is equipped to a <img src='img/text/fed-constitution-refit.png' width='20' height='20'>. \n\n Your fleet may include 1 additional <img src='img/text/faction-federation.png' width='20' height='20' title='Federation'> Admiral with a cost of 4 SP of less. Both Admirals may not be equipped to the same ship.",
+		factions: [
+			"federation"
+		],
+		cost: 5,
+		skill: 0,
+		range: "1 - 2"
+	},
+	{
+		type: "admiral",
+		id: "A044",
+		gameId: 1,
+		set: [
+			"75016"
+		],
 		name: "James T. Kirk",
 		unique: true,
 		text: "Add 2 [crew_text] to this ship's Upgrade Bar <hr> <b>WHEN DEFENDING:</b> If this ship is within Range of a Non-Minefield Obstacle. \n\n Roll +1 defense die. In addition if the attacking ship is either a <img src='img/text/faction-klingon.png' width='20' height='20' title='Klingon'> ship or <img src='img/text/faction-federation.png' width='20' height='20' title='Federation'> ship, you may re-roll 1 [blank]. <hr> <b>GATHER FORCES STEP:</b> If this card is equipped to a <img src='img/text/fed-constitution-refit.png' width='20' height='20'>. \n\n Your fleet may include 1 additional <img src='img/text/faction-federation.png' width='20' height='20' title='Federation'> Admiral with a cost of 4 SP of less. Both Admirals may not be equipped to the same ship.",
